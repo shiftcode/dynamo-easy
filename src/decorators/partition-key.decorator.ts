0e2569e11439505548487879e2e8bcf0e5fa3db8
Michael Wittwer <michael.wittwer@shiftcode.ch>
2017-08-16 11:53:27 +0200

    remove unused (merge issue)

--- src/decorators/partition-key.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { PropertyMetadata } from './property-metadata.model'
-import { initOrUpdateProperty, KEY_PROPERTY } from './property.decorator'
-
-// FIXME check for type of partition key only some scalars are allowed
-export function PartitionKey(): PropertyDecorator {
-  return function(target: Object, propertyKey: string) {
-    initOrUpdateProperty({ partitionKey: true }, target, propertyKey)
-  }
-}
