63c2f9de17929153b6413d8fe4feb8d2de969c7e
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2018-11-28 19:01:32 +0100

    test(scan-request): add test for log statements

--- src/dynamo/request/scan/scan.request.spec.ts
+++ src/dynamo/request/scan/scan.request.spec.ts
@@ -2,6 +2,8 @@ import { ScanInput, ScanOutput } from 'aws-sdk/clients/dynamodb'
 import { Observable, of } from 'rxjs'
 import { getTableName } from '../../../../test/helper'
 import { ComplexModel } from '../../../../test/models'
+import { updateDynamoEasyConfig } from '../../../config'
+import { LogInfo } from '../../../logger'
 import { Request } from '../request.model'
 import { ScanRequest } from './scan.request'
 
@@ -11,12 +13,16 @@ class DynamoRxMock {
   }
 }
 
+let logInfos: LogInfo[]
+updateDynamoEasyConfig({ logReceiver: item => logInfos.push(item) })
+
 describe('scan request', () => {
   const dynamoRxMock: DynamoRxMock = new DynamoRxMock()
   let scanRequest: ScanRequest<any>
 
   beforeEach(() => {
     scanRequest = new ScanRequest(<any>dynamoRxMock, ComplexModel, getTableName(ComplexModel))
+    logInfos = []
   })
 
   it('default params', () => {
@@ -24,5 +30,10 @@ describe('scan request', () => {
     expect(params.TableName).toBe('complex_model')
     expect(params.Limit).toBe(Request.DEFAULT_LIMIT)
     expect(Object.keys(params).length).toBe(2)
+  })
+
+  it('should log', async () => {
+    await scanRequest.exec().toPromise()
+    expect(logInfos.length).toBeGreaterThan(0)
   })
 })
