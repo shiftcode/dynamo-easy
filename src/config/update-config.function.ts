33240a2c8abb106c544fd590658c2718434d5480
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2018-11-28 19:17:18 +0100

    fix(update-config): fix check if dateMapper arg is invalid

--- src/config/update-config.function.ts
+++ src/config/update-config.function.ts
@@ -5,7 +5,7 @@ export function updateDynamoEasyConfig(config: Partial<Config>): void {
   if (config.logReceiver !== undefined && typeof config.logReceiver !== 'function') {
     throw new Error('Config.logReceiver has to be a function')
   }
-  if(!config.dateMapper || typeof config.dateMapper.toDb !== 'function' || typeof config.dateMapper.fromDb !== 'function') {
+  if (config.dateMapper !== undefined && (config.dateMapper === null || typeof config.dateMapper.toDb !== 'function' || typeof config.dateMapper.fromDb !== 'function')) {
     throw new Error('Config.dateMapper must be an object of type MapperForType')
   }
   Object.assign(dynamoEasyConfig, config)
