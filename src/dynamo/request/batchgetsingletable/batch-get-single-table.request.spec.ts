b2a2b387d077facfe1eb54e3df29389b572e2500
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2018-11-29 11:00:31 +0100

    test(batch-get-single-table-request): add tests for constructor and logger

--- src/dynamo/request/batchgetsingletable/batch-get-single-table.request.spec.ts
+++ src/dynamo/request/batchgetsingletable/batch-get-single-table.request.spec.ts
@@ -1,9 +1,48 @@
+import { of } from 'rxjs'
+// tslint:disable:no-unnecessary-class
+// tslint:disable:no-unused-expression
 import { getTableName } from '../../../../test/helper'
-import { Organization } from '../../../../test/models'
+import {
+  Organization,
+  SimpleWithCompositePartitionKeyModel,
+  SimpleWithPartitionKeyModel,
+} from '../../../../test/models'
+import { updateDynamoEasyConfig } from '../../../config'
 import { BatchGetSingleTableRequest } from './batch-get-single-table.request'
 
 describe('batch get', () => {
+
+  describe('constructor', () => {
+
+    it('should throw when no class was given', () => {
+      expect(() => new BatchGetSingleTableRequest(<any>null, <any>null, 'tableName', [])).toThrow()
+    })
+
+    xit('should throw when a class without metadata was given', () => {
+      class NoModelClass {}
+
+      expect(() => new BatchGetSingleTableRequest(<any>null, NoModelClass, 'tableName', [])).toThrow()
+    })
+
+    it('should throw when adding a key without partition key', () => {
+      expect(() => {
+        new BatchGetSingleTableRequest(<any>null, SimpleWithPartitionKeyModel, 'tableName', [{}])
+      }).toThrow()
+      expect(() => {
+        new BatchGetSingleTableRequest(<any>null, SimpleWithPartitionKeyModel, 'tableName', [undefined])
+      }).toThrow()
+    })
+
+    xit('should throw when sort key is missing', () => {
+      expect(() => {
+        new BatchGetSingleTableRequest(<any>null, SimpleWithCompositePartitionKeyModel, 'tableName', [{ partitionKey: 'aKey' }])
+      }).toThrow()
+    })
+
+  })
+
   describe('correct params', () => {
+
     it('simple primary key', () => {
       const request = new BatchGetSingleTableRequest<any>(<any>null, Organization, getTableName(Organization), [
         'myId',
@@ -31,5 +70,38 @@ describe('batch get', () => {
         },
       })
     })
+
   })
+
+  describe('logger', () => {
+    let logReceiverSpy: jasmine.Spy
+    let batchGetItemsSpy: jasmine.Spy
+    let req: BatchGetSingleTableRequest<SimpleWithPartitionKeyModel>
+
+    const sampleResponse = { Responses: null }
+
+    beforeEach(() => {
+      logReceiverSpy = jasmine.createSpy()
+      batchGetItemsSpy = jasmine.createSpy().and.returnValue(of(sampleResponse))
+      updateDynamoEasyConfig({ logReceiver: logReceiverSpy })
+      req = new BatchGetSingleTableRequest(<any>{ batchGetItems: batchGetItemsSpy }, SimpleWithPartitionKeyModel, getTableName(SimpleWithPartitionKeyModel), [])
+    })
+
+    it('exec should log params and response', async () => {
+      await req.exec().toPromise()
+      expect(logReceiverSpy).toHaveBeenCalled()
+      const logInfoData = logReceiverSpy.calls.allArgs().map(i => i[0].data)
+      expect(logInfoData.includes(req.params)).toBeTruthy()
+      expect(logInfoData.includes(sampleResponse)).toBeTruthy()
+    })
+
+    it('execFullResponse should log params and response', async () => {
+      await req.execFullResponse().toPromise()
+      expect(logReceiverSpy).toHaveBeenCalled()
+      const logInfoData = logReceiverSpy.calls.allArgs().map(i => i[0].data)
+      expect(logInfoData.includes(req.params)).toBeTruthy()
+      expect(logInfoData.includes(sampleResponse)).toBeTruthy()
+    })
+  })
+
 })
