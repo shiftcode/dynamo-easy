99d3baf0ef60878e8e4c03e914a9560ef5b59749
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2018-11-29 09:29:56 +0100

    test(dynamo-store): add tests for session validity ensurer + logger

--- src/dynamo/dynamo-store.spec.ts
+++ src/dynamo/dynamo-store.spec.ts
@@ -1,6 +1,10 @@
+import { EMPTY } from 'rxjs'
+import { updateDynamoEasyConfig } from '../config'
 // tslint:disable:max-classes-per-file
 // tslint:disable:no-unnecessary-class
+// tslint:disable:no-unused-expression
 import { Model } from '../decorator/impl'
+import { DEFAULT_TABLE_NAME_RESOLVER } from './default-table-name-resolver.const'
 import { DynamoStore } from './dynamo-store'
 
 @Model()
@@ -10,30 +14,51 @@ class DynamoStoreModel {}
 class DynamoStoreModel2 {}
 
 describe('dynamo store', () => {
-  describe('table name', () => {
-    it('correct table name (default)', () => {
-      const dynamoStore = new DynamoStore(DynamoStoreModel)
 
+  describe('table name', () => {
+    it('correct table name - default', () => {
+      const dynamoStore = new DynamoStore(DynamoStoreModel)
       expect(dynamoStore.tableName).toBe('dynamo-store-models')
     })
 
-    it('correct table name ()', () => {
+    it('correct table name - by decorator', () => {
       const dynamoStore = new DynamoStore(DynamoStoreModel2)
-
       expect(dynamoStore.tableName).toBe('myTableName')
     })
 
-    it('correct table name ()', () => {
+    it('correct table name - by tableNameResolver', () => {
       const dynamoStore = new DynamoStore(DynamoStoreModel2, tableName => `${tableName}-with-special-thing`)
-
       expect(dynamoStore.tableName).toBe('myTableName-with-special-thing')
     })
 
     it('throw error because table name is invalid', () => {
-      expect(() => {
-        // tslint:disable-next-line:no-unused-expression
-        new DynamoStore(DynamoStoreModel2, tableName => `${tableName}$`)
-      }).toThrowError()
+      // tslint:disable-next-line:no-unused-expression
+      expect(() => new DynamoStore(DynamoStoreModel2, tableName => `${tableName}$`)).toThrowError()
     })
   })
+
+  describe('session validity ensurer', () => {
+    let validityEnsurerSpy: jasmine.Spy
+    beforeEach(() => {
+      validityEnsurerSpy = jasmine.createSpy().and.returnValue(EMPTY)
+    })
+    it('custom session validity ensurer is used', async () => {
+      const store = new DynamoStore(DynamoStoreModel, DEFAULT_TABLE_NAME_RESOLVER, validityEnsurerSpy)
+      await store.scan().exec().toPromise()
+      expect(validityEnsurerSpy).toHaveBeenCalled()
+    })
+  })
+
+  describe('logger', () => {
+    let logReceiverSpy: jasmine.Spy
+    beforeEach(() => {
+      logReceiverSpy = jasmine.createSpy()
+      updateDynamoEasyConfig({ logReceiver: logReceiverSpy })
+    })
+    it('logs when instance was created', () => {
+      new DynamoStore(DynamoStoreModel)
+      expect(logReceiverSpy).toHaveBeenCalled()
+    })
+  })
+
 })
