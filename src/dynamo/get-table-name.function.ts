7e3db5f62a54e8bfee54d163c3211e62c6046ad4
Michael Wittwer <michael.wittwer@shiftcode.ch>
2018-12-06 14:32:53 +0100

    refactor(get-table-function): move regex where it belongs

--- src/dynamo/get-table-name.function.ts
+++ src/dynamo/get-table-name.function.ts
@@ -1,8 +1,12 @@
 import { dynamoEasyConfig } from '../config/dynamo-easy-config'
 import { Metadata, metadataForClass } from '../decorator/metadata'
 import { ModelConstructor } from '../model'
-import { REGEX_TABLE_NAME } from './request/regex'
 
+/*
+ * only contains these characters «a-z A-Z 0-9 - _ .» and is between 3 and 255 characters long
+ * http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Limits.html#limits-naming-rules
+ */
+const REGEX_TABLE_NAME = /^[a-zA-Z0-9_\-.]{3,255}$/
 
 export function getTableName(metaDataOrModelClazz: Metadata<any> | ModelConstructor<any>): string {
   const modelOptions = metaDataOrModelClazz instanceof Metadata
@@ -13,7 +17,7 @@ export function getTableName(metaDataOrModelClazz: Metadata<any> | ModelConstruc
 
   if (!REGEX_TABLE_NAME.test(tableName)) {
     throw new Error(
-      'make sure the table name only contains these characters «a-z A-Z 0-9 - _ .» and is between 3 and 255 characters long',
+      `make sure the table name «${tableName}» is valid (see http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Limits.html#limits-naming-rules for details)`,
     )
   }
   return tableName
