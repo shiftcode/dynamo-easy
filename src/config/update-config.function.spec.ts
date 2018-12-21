20e97b5a7a34708ad315b62fa53ce5c10616a0ae
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2018-12-21 15:18:24 +0100

    fix(update-config.function): fix and correctly test the function

--- src/config/update-config.function.spec.ts
+++ src/config/update-config.function.spec.ts
@@ -8,14 +8,37 @@ import { updateDynamoEasyConfig } from './update-config.function'
 describe('updateDynamoEasyConfig', () => {
   afterEach(resetDynamoEasyConfig)
 
-  it('should throw when providing invalid stuff', () => {
-    expect(() => updateDynamoEasyConfig({ logReceiver: <any>null })).toThrow()
-    expect(() => updateDynamoEasyConfig({ dateMapper: <any>null })).toThrow()
-  })
-
   it('should have defaults', () => {
     expect(dynamoEasyConfig.logReceiver).toBeDefined()
     expect(dynamoEasyConfig.dateMapper).toBeDefined()
+    expect(dynamoEasyConfig.tableNameResolver).toBeDefined()
+    expect(dynamoEasyConfig.sessionValidityEnsurer).toBeDefined()
+  })
+
+  it('should throw when providing explicitly undefined, null, or a non-function value', () => {
+    expect(() => updateDynamoEasyConfig({ logReceiver: undefined })).toThrow()
+    expect(() => updateDynamoEasyConfig({ logReceiver: <any>null })).toThrow()
+    expect(() => updateDynamoEasyConfig({ logReceiver: <any>'foo' })).toThrow()
+
+    expect(() => updateDynamoEasyConfig({ dateMapper: undefined })).toThrow()
+    expect(() => updateDynamoEasyConfig({ dateMapper: <any>null })).toThrow()
+    expect(() => updateDynamoEasyConfig({ dateMapper: <any>'foo' })).toThrow()
+
+    expect(() => updateDynamoEasyConfig({ tableNameResolver: undefined })).toThrow()
+    expect(() => updateDynamoEasyConfig({ tableNameResolver: <any>null })).toThrow()
+    expect(() => updateDynamoEasyConfig({ tableNameResolver: <any>'foo' })).toThrow()
+
+    expect(() => updateDynamoEasyConfig({ sessionValidityEnsurer: undefined })).toThrow()
+    expect(() => updateDynamoEasyConfig({ sessionValidityEnsurer: <any>null })).toThrow()
+    expect(() => updateDynamoEasyConfig({ sessionValidityEnsurer: <any>'foo' })).toThrow()
+  })
+
+  it('should not override default when updating but not providing a value', () => {
+    updateDynamoEasyConfig({})
+    expect(dynamoEasyConfig.logReceiver).toBeDefined()
+    expect(dynamoEasyConfig.dateMapper).toBeDefined()
+    expect(dynamoEasyConfig.tableNameResolver).toBeDefined()
+    expect(dynamoEasyConfig.sessionValidityEnsurer).toBeDefined()
   })
 
   it('should work when providing valid stuff', () => {
