334d2049d0ba7c304d34c1a5c9bab28907655cb6
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2018-12-21 19:45:34 +0100

    feat(transact-update): add updateAttribute equivalent to UpdateRequest

--- src/dynamo/transactwrite/transact-update.ts
+++ src/dynamo/transactwrite/transact-update.ts
@@ -2,13 +2,18 @@ import * as DynamoDB from 'aws-sdk/clients/dynamodb'
 import { createKeyAttributes } from '../../mapper'
 import { ModelConstructor } from '../../model'
 import { prepareAndAddUpdateExpressions } from '../expression/prepare-and-add-update-expressions.function'
-import { UpdateExpressionDefinitionFunction } from '../expression/type'
+import { addUpdate } from '../expression/request-expression-builder'
+import { RequestUpdateFunction, UpdateExpressionDefinitionFunction } from '../expression/type'
 import { TransactBaseOperation } from './transact-base-operation'
 
 export class TransactUpdate<T> extends TransactBaseOperation<T, DynamoDB.Update, TransactUpdate<T>> {
   constructor(modelClazz: ModelConstructor<T>, partitionKey: any, sortKey?: any) {
     super(modelClazz)
     this.params.Key = createKeyAttributes(this.metadata, partitionKey, sortKey)
+  }
+
+  updateAttribute<K extends keyof T>(attributePath: K): RequestUpdateFunction<TransactUpdate<T>, T, K> {
+    return addUpdate(attributePath, this, this.metadata)
   }
 
   operations(...updateDefFns: UpdateExpressionDefinitionFunction[]): TransactUpdate<T> {
