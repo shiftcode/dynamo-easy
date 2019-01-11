13ad560746509a7289725bada632218921e15d0a
Michael Wittwer <michael.wittwer@shiftcode.ch>
2019-01-11 10:06:34 +0100

    refactor(model-construtor): consistent naming
    change all modelClass occurrences to modelConstructor

--- src/decorator/impl/collection/typed-array.decorator.ts
+++ src/decorator/impl/collection/typed-array.decorator.ts
@@ -3,10 +3,10 @@ import { TypeInfo } from '../../metadata/property-metadata.model'
 import { initOrUpdateProperty } from '../property/init-or-update-property.function'
 
 /**
- * Makes sure the property will be mapped to a L(ist) type. The modelClass is required if the collection items
+ * Makes sure the property will be mapped to a L(ist) type. The modelConstructor is required if the collection items
  * have some property decorators, so we can retrieve this information using the model class.
  */
-export function TypedArray<T>(modelClass?: ModelConstructor<T>): PropertyDecorator {
+export function TypedArray<T>(modelConstructor?: ModelConstructor<T>): PropertyDecorator {
   return (target: object, propertyKey: string | symbol) => {
     if (typeof propertyKey === 'string') {
       const typeInfo: TypeInfo = {
@@ -14,8 +14,8 @@ export function TypedArray<T>(modelClass?: ModelConstructor<T>): PropertyDecorat
         isCustom: true,
       }
 
-      if (modelClass) {
-        typeInfo.genericType = modelClass
+      if (modelConstructor) {
+        typeInfo.genericType = modelConstructor
       }
 
       initOrUpdateProperty({ typeInfo }, target, propertyKey)
