dbb1da26aac30ab8abf4fdd29bb9d26faa10c3c3
Michael Wittwer <michael.wittwer@shiftcode.ch>
2026-03-19 10:14:15 +0100

    refactor: replace yml config files with js

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+/**
+ * @see https://github.com/lint-staged/lint-staged?tab=readme-ov-file#typescript
+ * @type  { import('lint-staged').Configuration }
+ */
+const config = {
+  "package.json": ["npx sort-package-json"],
+  "*": ["eslint --fix --cache", "npx prettier --write --ignore-unknown"],
+};
+
+export default config;
---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type { import('prettier').Config }
+ */
+const config = {
+  printWidth: 120,
+  singleQuote: true,
+  semi: false,
+  trailingComma: "all",
+};
+
+export default config;
