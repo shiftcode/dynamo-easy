f9fc1fc73e229a8c3bdcb61296bdd6c74f157123
Michael Wittwer <michael.wittwer@shiftcode.ch>
2026-03-19 10:23:06 +0100

    feat: activate eslint during lint staged

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "./tsconfig.jest.json",
+      },
+    ],
+  },
+  setupFiles: ["reflect-metadata", "./test/jest-setup.ts"],
+};
